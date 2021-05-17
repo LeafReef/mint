58f43cd6e40245e500126789af5479df693fe656
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-05-17 19:22:24 +0300

    Add temperature and humidity

---
+++ scripts/fetchSensorData.js
@@ -0,0 +1,13 @@
+const URL = 'https://leafreef.herokuapp.com/api/list/';
+
+async function fetchSensorData() {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const { temperature, humidity } = data;
+
+    document.getElementById('temp').textContent = temperature + '°C';
+    document.getElementById('hum').textContent = humidity + '%';
+}
+
+fetchSensorData();
+setInterval(fetchSensorData, 1000);
